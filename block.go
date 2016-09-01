8d59016d2c9901a76e6512afbe02aabf7e5011a2
Koichi Shiraishi <zchee.io@gmail.com>
2016-09-01 19:10:50 +0900

    create: add Create function
    - not finish because not implements of write the L1 header and data
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- block.go
+++ block.go
@@ -4,67 +4,75 @@
 
 package qcow2
 
-import "os"
-
-// Driver represents a name of driver.
-type Driver string
-
-const (
-	// DriverQCow2 qcow2 driver.
-	DriverQCow2 Driver = "qcow2"
+import (
+	"bytes"
+	"log"
+	"os"
 )
 
 // BlockOption represents a block options.
 type BlockOption struct {
-	Driver Driver
-}
-
-// NewBlockOption return the new BlockOption.
-func NewBlockOption(driver Driver) *BlockOption {
-	return &BlockOption{
-		Driver: driver,
-	}
+	Driver BlockDriver
 }
 
 // BlockBackend represents a backend of the QCow2 image format block driver.
 type BlockBackend struct {
-	img            *os.File
-	header         *QCowHeader
-	allowBeyondEOF bool
+	File             *os.File
+	Header           Header
+	allowBeyondEOF   bool
+	BlockDriverState *BlockDriverState
+
+	buf bytes.Buffer
 
 	Error error
 }
 
-// NewBlockBackend return the new bulk structure.
-func NewBlockBackend(header *QCowHeader, disk *os.File) *BlockBackend {
-	return &BlockBackend{
-		header: header,
-		img:    disk,
-	}
-}
-
 // Open open the QCow2 block-backend image file.
 func (blk *BlockBackend) Open(filename, reference string, options *BlockOption, flag int) error {
-	image, err := os.OpenFile(filename, flag, os.FileMode(0))
+	file, err := os.OpenFile(filename, flag, os.FileMode(0))
 	if err != nil {
 		return err
 	}
 
-	blk.img = image
-
-	// drv := "protocol?"
-	// if drv == "" {
-	// 	err := errors.New("unknown protocol")
-	// 	return err
-	// }
-
-	// f, err := os.Open(filename)
-	// if err != nil {
-	// 	return errors.Wrap(err, "invalid filename")
-	// }
-	// AllowWriteBeyondEOF = true
-
-	// header := &QCowHeader{}
+	blk.File = file
 
 	return nil
+}
+
+func PrintByte(buf []byte) {
+	log.Println(buf)
+	log.Printf("    [0:3] Magic:                 %+v", buf[:4])
+	log.Printf("    [4:7] Version:               %+v", buf[4:8])
+	log.Printf("   [8:15] BackingFileOffset:     %+v", buf[8:16])
+	log.Printf("  [16:19] BackingFileSize:       %+v", buf[16:20])
+	log.Printf("  [20:23] ClusterBits:           %+v", buf[20:24])
+	log.Printf("  [24:31] Size:                  %+v", buf[24:32])
+	log.Printf("  [32:35] CryptMethod:           %+v", buf[32:36])
+	log.Printf("  [36:39] L1Size:                %+v", buf[36:40])
+	log.Printf("  [40:47] L1TableOffset:         %+v", buf[40:48])
+	log.Printf("  [48:55] RefcountTableOffset:   %+v", buf[48:56])
+	log.Printf("  [56:59] RefcountTableClusters: %+v", buf[56:60])
+	log.Printf("  [60:63] NbSnapshots:           %+v", buf[60:64])
+	log.Printf("  [64:71] SnapshotsOffset:       %+v", buf[64:72])
+	log.Printf("  [72:79] IncompatibleFeatures:  %+v", buf[72:80])
+	log.Printf("  [80:87] CompatibleFeatures:    %+v", buf[80:88])
+	log.Printf("  [88:95] AutoclearFeatures:     %+v", buf[88:96])
+	log.Printf("  [96:99] RefcountOrder:         %+v", buf[96:100])
+	log.Printf("[101:105] HeaderLength:          %+v", buf[100:104])
+
+	log.Printf("[106:109] HeaderExtensionType:   %+v", buf[104:108])
+	log.Printf("[110:114] HeaderExtensionLength: %+v", buf[108:112])
+	log.Printf("[115:162] HeaderExtensionData:   %+v", buf[112:158])
+
+	log.Printf("[163:166] HeaderExtensionType:   %+v", buf[158:162])
+	log.Printf("[167:171] HeaderExtensionLength: %+v", buf[162:166])
+	log.Printf("[172:219] HeaderExtensionData:   %+v", buf[166:212])
+
+	log.Printf("[220:223] HeaderExtensionType:   %+v", buf[212:216])
+	log.Printf("[224:228] HeaderExtensionLength: %+v", buf[216:220])
+	log.Printf("[229:276] HeaderExtensionData:   %+v", buf[220:266])
+
+	if len(buf) > 266 {
+		log.Printf("[277:]    Other:                 %+v", buf[277:len(buf)-1])
+	}
 }
--- create.go
+++ create.go
@@ -5,161 +5,305 @@
 package qcow2
 
 import (
+	"bytes"
+	"encoding/binary"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"syscall"
+	"unsafe"
+
+	"github.com/pkg/errors"
 )
 
-// "compat"
-//  "compat=0.10": uses the traditional image format that can be read by any QEMU since 0.10.
-//  "compat=1.1":  enables image format extensions that only QEMU 1.1 and newer understand (this is the default).
-//
-// "backing_file"
-//  File name of a base image (see create subcommand)
-//
-// "backing_fmt"
-//  Image format of the base image
-//
-// "encryption"
-//  If this option is set to "on", the image is encrypted with 128-bit AES-CBC.
-//
-// "cluster_size"
-//  Changes the qcow2 cluster size (must be between 512 and 2M).
-//  Smaller cluster sizes can improve the image file size whereas larger cluster sizes generally provide better performance.
-//
-// "preallocation"
-//  Preallocation mode (allowed values: "off", "metadata", "falloc", "full").
-//  An image with preallocated metadata is initially larger but can improve performance when the image needs to grow.
-//  "falloc" and "full" preallocations are like the same options of "raw" format, but sets up metadata also.
-//
-// "lazy_refcounts"
-//  If this option is set to "on", reference count updates are postponed with the goal of avoiding metadata I/O and improving performance.
-//  This is particularly interesting with cache=writethrough which doesn't batch metadata updates.
-//  The tradeoff is that after a host crash, the reference count tables must be rebuilt,
-//  i.e. on the next open an (automatic) "qemu-img check -r all" is required, which may take some time.
-//  This option can only be enabled if "compat=1.1" is specified.
-//
-// "nocow"
-//  If this option is set to "on", it will turn off COW of the file. It's only valid on btrfs, no effect on other file systems.
-//  Btrfs has low performance when hosting a VM image file, even more when the guest on the VM also using btrfs as file system.
-//  Turning off COW is a way to mitigate this bad performance. Generally there are two ways to turn off COW on btrfs: a)
-//  Disable it by mounting with nodatacow, then all newly created files will be NOCOW. b)
-//  For an empty file, add the NOCOW file attribute. That's what this option does.
-//  Note: this option is only valid to new or empty files.
-//  If there is an existing file which is COW and has data blocks already, it couldn't be changed to NOCOW by setting "nocow=on".
-//  One can issue "lsattr filename" to check if the NOCOW flag is set or not (Capital 'C' is NOCOW flag).
+// QemuOpts configs the create new qcow2 format image.
+type Opts struct {
+	// BLOCK_OPT
+	Size int64
 
-// Config configs the create new qcow2 format image.
-type Config struct {
-	FileName      string
-	TotalSize     int
-	Flags         int
-	Version       Version
-	RefcountOrder int
-
-	// Command line options
-	// Compat QCow2 image format compatible. values are 0.10 or 1.1(defaut).
-	Compat float64
-	// BackingFile File name of a base image.
-	BackingFile string
-	// BackingFormat Image format of the base image.
-	BackingFormat string
-	// Encryption Use 128-bit AES-CBC image encryption.
+	//  Encryption option is if this option is set to "on", the image is encrypted with 128-bit AES-CBC.
 	Encryption bool
-	// ClusterSize Must be between 512 and 2M.
+
+	Compat6 string
+
+	//  BackingFile file name of a base image (see create subcommand).
+	BackingFile string
+
+	//  BackingFormat image format of the base image.
+	BackingFormat string
+
+	//  ClusterSize option is changes the qcow2 cluster size (must be between 512 and 2M).
+	//  Smaller cluster sizes can improve the image file size whereas larger cluster sizes generally provide better performance.
 	ClusterSize int
-	// Preallocation Metadata preallocation mode.
-	Preallocation PreallocationMode
-	// LazyRefcounts Avoiding metadata I/O and improving performance with the postponed updates reference count.
-	LazyRefcounts bool
-	// NoCow whether turn off COW of the file. only valid on btrfs.
+
+	TableSize int
+
+	//  Preallocation mode of pre-allocation metadata (allowed values: "off", "metadata", "falloc", "full").
+	//  An image with preallocated metadata is initially larger but can improve performance when the image needs to grow.
+	//  "falloc" and "full" preallocations are like the same options of "raw" format, but sets up metadata also.
+	Preallocation PreallocMode
+
+	SubFormat string
+
+	//  Compat QCow2 image format compatible. "compat=0.10": uses the traditional image format that can be read by any QEMU since 0.10.
+	//  "compat=1.1":  enables image format extensions that only QEMU 1.1 and newer understand (this is the default).
+	Compat string
+
+	//  LazyRefcounts option is if this option is set to "on", reference count updates are postponed with the goal of avoiding metadata I/O and improving performance.
+	//  This is particularly interesting with cache=writethrough which doesn't batch metadata updates.
+	//  The tradeoff is that after a host crash, the reference count tables must be rebuilt,
+	//  i.e. on the next open an (automatic) "qemu-img check -r all" is required, which may take some time.
+	//  This option can only be enabled if "compat=1.1" is specified.
+	LazyRefcounts bool // LazyRefcounts Avoiding metadata I/O and improving performance with the postponed updates reference count.
+
+	AdapterType string
+
+	Redundancy bool
+
+	//  NoCow option is if this option is set to "on", it will turn off COW of the file. It's only valid on btrfs, no effect on other file systems.
+	//  Btrfs has low performance when hosting a VM image file, even more when the guest on the VM also using btrfs as file system.
+	//  Turning off COW is a way to mitigate this bad performance. Generally there are two ways to turn off COW on btrfs: a)
+	//  Disable it by mounting with nodatacow, then all newly created files will be NOCOW. b)
+	//  For an empty file, add the NOCOW file attribute. That's what this option does.
+	//  Note: this option is only valid to new or empty files.
+	//  If there is an existing file which is COW and has data blocks already, it couldn't be changed to NOCOW by setting "nocow=on".
+	//  One can issue "lsattr filename" to check if the NOCOW flag is set or not (Capital 'C' is NOCOW flag).
 	NoCow bool
+
+	ObjectSize int
+
+	RefcountBits int
 }
 
-// Create create the new QCow2 virtual disk image.
-//  Docker.qcow2[0:104]:
-//  81 70 73 251 0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 16 0 0 0 16 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 2 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 4 0 0 0 104
-func Create(cfg *Config) *QCowHeader {
-	// clusterBits := ToBigEndian32(cfg.ClusterSize)
-	// if clusterBits < MinClusterBits || clusterBits > MaxClusterBits || (1<<uintptr(clusterBits)) != cfg.ClusterSize {
-	// 	_ = errors.Errorf("Cluster size must be a power of two between %d and %dk", 1<<MinClusterBits, 1<<(MaxClusterBits-10))
-	// 	return nil
-	// }
+// Create creates the new QCow2 virtual disk image by the qemu style.
+func Create(filename string, opts *Opts) (Header, error) {
+	var (
+		flags   int
+		version = Version3
+		hdrbuf  bytes.Buffer
+	)
 
-	// TODO(zchee): Support (prealloc == PREALLOC_MODE_FULL || prealloc == PREALLOC_MODE_FALLOC)
-	// if (prealloc == PREALLOC_MODE_FULL || prealloc == PREALLOC_MODE_FALLOC) {}
+	size := roundUp(int(opts.Size), BDRV_SECTOR_SIZE)
+	backingFile := opts.BackingFile
+	backingFormat := opts.BackingFormat
+	log.Printf("backingFormat: %+v\n", backingFormat)
+	if opts.Encryption {
+		flags |= BLOCK_FLAG_ENCRYPT
+	}
+	clusterSize := opts.ClusterSize
+	if clusterSize == 0 {
+		clusterSize = DEFAULT_CLUSTER_SIZE
+	}
+	// TODO(zchee): error handle
+	prealloc := opts.Preallocation
+
+	compat := opts.Compat
+	switch compat {
+	case "0.10":
+		version = Version2
+	case "1.1":
+		// nothing to do
+	case "":
+		compat = "1.1"
+	default:
+		err := errors.Errorf("Invalid compatibility level: '%s'", compat)
+		return Header{}, err
+	}
+
+	if opts.LazyRefcounts {
+		flags |= BLOCK_FLAG_LAZY_REFCOUNTS
+	}
+
+	if backingFile != "" && prealloc != PREALLOC_MODE_OFF {
+		err := errors.New("Backing file and preallocation cannot be used at the same time")
+		return Header{}, err
+	}
+
+	if version < 3 && (flags&BLOCK_FLAG_LAZY_REFCOUNTS) == 0 {
+		err := errors.New("Lazy refcounts only supported with compatibility level 1.1 and above (use compat=1.1 or greater)")
+		return Header{}, err
+	}
+
+	refcountBits := opts.RefcountBits
+	if refcountBits == 0 {
+		refcountBits = 16 // defaults
+	}
+	if refcountBits > 64 {
+		err := errors.New("Refcount width must be a power of two and may not exceed 64 bits")
+		return Header{}, err
+	}
+
+	refcountOrder := ctz32(uint32(refcountBits))
+
+	clusterBits := ctz32(uint32(clusterSize))
+	if clusterBits < MIN_CLUSTER_BITS || clusterBits > MAX_CLUSTER_BITS || (1<<uint(clusterBits)) != opts.ClusterSize {
+		err := errors.Errorf("Cluster size must be a power of two between %d and %dk", 1<<MIN_CLUSTER_BITS, 1<<(MAX_CLUSTER_BITS-10))
+		return Header{}, err
+	}
+
+	if prealloc == PREALLOC_MODE_FULL || prealloc == PREALLOC_MODE_FALLOC {
+		// Note: The following calculation does not need to be exact; if it is a
+		// bit off, either some bytes will be "leaked" (which is fine) or we
+		// will need to increase the file size by some bytes (which is fine,
+		// too, as long as the bulk is allocated here). Therefore, using
+		// floating point arithmetic is fine.
+		var metaSize int64
+		alignedTotalZize := alignOffset(size, clusterSize)
+		rces := int64(1<<uint(refcountOrder)) / 8.
+
+		refblockBits := clusterBits - (refcountOrder - 3)
+		refblockSize := 1 << uint(refblockBits)
+
+		metaSize += int64(clusterSize)
+
+		nl2e := alignedTotalZize / int64(clusterSize)
+		nl2e = alignOffset(nl2e, clusterSize/UINT64_SIZE)
+		metaSize += nl2e * UINT64_SIZE
+
+		nl1e := nl2e * UINT64_SIZE / int64(clusterSize)
+		nl1e = alignOffset(nl1e, clusterSize/UINT64_SIZE)
+		metaSize += nl1e * UINT64_SIZE
+
+		// total size of refcount blocks
+		//
+		// note: every host cluster is reference-counted, including metadata
+		// (even refcount blocks are recursively included).
+		// Let:
+		//   a = total_size (this is the guest disk size)
+		//   m = meta size not including refcount blocks and refcount tables
+		//   c = cluster size
+		//   y1 = number of refcount blocks entries
+		//   y2 = meta size including everything
+		//   rces = refcount entry size in bytes
+		// then,
+		//   y1 = (y2 + a)/c
+		//   y2 = y1 * rces + y1 * rces * sizeof(u64) / c + m
+		// we can get y1:
+		//   y1 = (a + m) / (c - rces - rces * sizeof(u64) / c)
+		nrefblocke := (alignedTotalZize + metaSize + int64(clusterSize)) / (int64(clusterSize) - rces - rces*UINT64_SIZE) / int64(clusterSize)
+		metaSize += divRoundUp(int(nrefblocke), refblockSize) * int64(clusterSize)
+
+		// total size of refcount tables
+		nreftablee := nrefblocke / int64(refblockSize)
+		nreftablee = alignOffset(nreftablee, clusterSize/UINT64_SIZE)
+		metaSize += nreftablee * UINT64_SIZE
+
+		size = alignedTotalZize + metaSize
+	}
 
 	blkOption := new(BlockOption)
-	image, err := CreateFile(cfg.FileName, blkOption)
+	image, err := CreateFile(filename, blkOption)
 	if err != nil {
-		log.Fatal(err)
+		return Header{}, err
 	}
 	defer image.Close()
 	defer os.Remove(image.Name())
 
 	blk := new(BlockBackend)
 	if err := blk.Open(image.Name(), "", nil, os.O_RDWR); err != nil {
-		log.Fatal(err)
+		return Header{}, err
 	}
 
 	blk.allowBeyondEOF = true
 
-	header := &QCowHeader{
-		Magic:                 QcowMagic,
-		Version:               Version3,
-		BackingFileOffset:     int64(0),
-		BackingFileSize:       int32(0),
-		ClusterBits:           int32(16),
-		Size:                  int64(cfg.TotalSize), // TODO(zchee): Sets to when initializing of the header? qemu is after initialization.
-		L1TableOffset:         int64(131072),        // TODO(zchee): ditto
-		L1Size:                int32(128),           // TODO(zchee): ditto
-		RefcountTableOffset:   int64(cfg.ClusterSize),
-		RefcountTableClusters: int32(1), // TODO(zchee): contant of 1?
-		IncompatibleFeatures:  int64(1),
-		RefcountOrder:         int32(4),
-		HeaderLength:          int32(Version3HeaderSize), // Sets 104 length by default.
+	blk.Header = Header{
+		Magic:                 BEUint32(MAGIC),
+		Version:               version,
+		BackingFileOffset:     uint64(0),
+		BackingFileSize:       uint32(0),
+		ClusterBits:           uint32(clusterBits),
+		Size:                  uint64(size), // TODO(zchee): Sets to when initializing of the header? qemu is after initialization.
+		CryptMethod:           CRYPT_NONE,
+		L1Size:                uint32(0),
+		L1TableOffset:         uint64(0),
+		RefcountTableOffset:   uint64(clusterSize),
+		RefcountTableClusters: uint32(1),
+		NbSnapshots:           uint32(0),
+		SnapshotsOffset:       uint64(0),
+		IncompatibleFeatures:  uint64(0),
+		CompatibleFeatures:    uint64(0),
+		AutoclearFeatures:     uint64(0),
+		RefcountOrder:         uint32(refcountOrder), // NOTE: qemu now supported only refcount_order = 4
+		HeaderLength:          uint32(unsafe.Sizeof(Header{})),
 	}
 
-	// Check the optional config
-	if cfg.BackingFile != "" {
-		f, err := os.Open(cfg.BackingFile) // read-only open.
-		if err != nil {
-			log.Fatal(err)
+	if opts.Encryption {
+		blk.Header.CryptMethod = CRYPT_AES
+	}
+
+	if opts.LazyRefcounts {
+		blk.Header.CompatibleFeatures |= uint64(COMPAT_LAZY_REFCOUNTS)
+	}
+
+	binary.Write(&hdrbuf, binary.BigEndian, blk.Header)
+
+	if blk.Header.Version >= Version3 {
+		binary.Write(&hdrbuf, binary.BigEndian, uint32(HeaderExtensionFeatureNameTable))
+
+		features := []Feature{
+			Feature{
+				Type: uint8(FEAT_TYPE_INCOMPATIBLE),
+				Bit:  uint8(INCOMPAT_DIRTY_BITNR),
+				Name: "dirty bit",
+			},
+			Feature{
+				Type: uint8(FEAT_TYPE_INCOMPATIBLE),
+				Bit:  uint8(INCOMPAT_CORRUPT_BITNR),
+				Name: "corrupt bit",
+			},
+			Feature{
+				Type: uint8(FEAT_TYPE_COMPATIBLE),
+				Bit:  uint8(COMPAT_LAZY_REFCOUNTS_BITNR),
+				Name: "lazy refcounts",
+			},
 		}
-		defer f.Close()
+		binary.Write(&hdrbuf, binary.BigEndian, uint32(unsafe.Sizeof(Feature{}))*uint32(len(features)))
 
-		data, err := ioutil.ReadAll(f)
-		if err != nil {
-			log.Fatal(err)
+		for _, f := range features {
+			binary.Write(&hdrbuf, binary.BigEndian, f.Type)
+			binary.Write(&hdrbuf, binary.BigEndian, f.Bit)
+			binary.Write(&hdrbuf, binary.BigEndian, []byte(f.Name))
+			zeroFill(&hdrbuf, int64(46-len([]byte(f.Name))))
 		}
-		header.BackingFileOffset = int64(len(data))
-
-		fstat, _ := f.Stat()
-		header.BackingFileSize = int32(fstat.Size())
-	}
-	if cfg.Encryption {
-		header.CryptMethod = CryptAES
-	}
-	if cfg.LazyRefcounts {
-		header.CompatibleFeatures = int64(CompatLazyRefcounts)
 	}
 
-	blk.header = header
-	if err := blk.WriteHeader(); err != nil {
-		log.Fatal(err)
+	blk.buf = hdrbuf
+
+	PrintByte(hdrbuf.Bytes())
+	return blk.Header, nil
+}
+
+func roundUp(n, d int) int64 {
+	return int64((n + d - 1) & -d)
+}
+
+func divRoundUp(n, d int) int64 {
+	return int64((n + d - 1) / d)
+}
+
+func alignOffset(offset int64, n int) int64 {
+	offset = int64((int(offset) + n - 1) & (n - 1))
+	return int64(offset)
+}
+
+// zeroFill writes n zero bytes into w.
+func zeroFill(w io.Writer, n int64) error {
+	const blocksize = 32 << 10
+	zeros := make([]byte, blocksize)
+	var k int
+	var err error
+	for n > 0 {
+		if n > blocksize {
+			k, err = w.Write(zeros)
+		} else {
+			k, err = w.Write(zeros[:n])
+		}
+		if err != nil {
+			return err
+		}
+		n -= int64(k)
 	}
-
-	options := NewBlockOption(DriverQCow2)
-	if err := blk.Open(image.Name(), "", options, os.O_RDWR|syscall.O_NONBLOCK); err != nil {
-		log.Fatal(err)
-	}
-	defer blk.img.Close()
-
-	data, _ := ioutil.ReadAll(blk.img)
-	log.Printf("data: %d\n%+v\n", len(data), data)
-
-	return header
+	return nil
 }
 
 // CreateFile creates the new file based by block driver backend.
